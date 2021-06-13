566826176d789337d8833e8da2fb7a09ccae3572
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-13 21:39:03 +0530

    Added: Linked list created

--- src/LinkedList/MyNodeTest.java
+++ src/LinkedList/MyNodeTest.java
@@ -1,22 +1,24 @@
 package LinkedList;
-	import org.junit.Test;
+import org.junit.Test;
 
-	import junit.framework.Assert;
+import junit.framework.Assert;
 
-	public class MyNodeTest {
-		@Test
-		public void given3NumbersWhenLinkedshouldPassedLinkedListTest() {
-			MyNode<Integer> MyFirstnode = new MyNode<Integer>( 56);
-			MyNode<Integer> MySecondnode = new MyNode<Integer>( 30);
-			MyNode<Integer> MyThirdnode = new MyNode<Integer>( 70);
-			MyFirstnode.setNext(MySecondnode);
-			MySecondnode.setNext(MyThirdnode);
-			boolean result = MyFirstnode.getNext().equals(MySecondnode) &&
-					MySecondnode.getNext().equals(MyThirdnode);
-			Assert.assertTrue(result);
-			
-			
-		}
+public class MyNodeTest {
+	@Test
+	public void given3NumbersWhenLinkedshouldPassedLinkedListTest() {
+		MyNode<Integer> MyFirstnode = new MyNode<Integer>( 56);
+		MyNode<Integer> MySecondnode = new MyNode<Integer>( 30);
+		MyNode<Integer> MyThirdnode = new MyNode<Integer>( 70);
+		MyFirstnode.setNext(MySecondnode);
+		MySecondnode.setNext(MyThirdnode);
+		boolean result = MyFirstnode.getNext().equals(MySecondnode) &&
+				MySecondnode.getNext().equals(MyThirdnode);
+		Assert.assertTrue(result);
+		
+		
+	}
+
+    }
 
 	}
 
