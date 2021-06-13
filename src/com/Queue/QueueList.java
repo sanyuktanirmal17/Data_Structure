ffce245ac8e92b7ff915d3d0695170d9f1d8934d
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-13 20:13:33 +0530

    Added: Queue with enqueue method

---
+++ src/com/Queue/QueueList.java
@@ -0,0 +1,59 @@
+package com.Queue;
+
+/*
+ * Queue operations 
+ */
+public class QueueList {
+		int[] queue = new int[3];
+		int front;
+		int rear;
+		int size;
+		/*
+		 * method add element 
+		 */
+
+		public void enQueue(int data) {
+			queue[rear] = data;
+			rear = (rear + 1) % 3;
+			size = size + 1;
+		}
+		/*
+		 * show method 
+		 */
+		
+		public void show() {
+			System.out.print("Element : ");
+			int length = queue.length;
+			int count = rear;
+		if (isFull()) {
+			while (length > 0) {
+					System.out.print(queue[count] + " ");
+					count++;
+					if (count == 3) {
+						count = 0;
+					}
+					length--;
+				}
+			} else {
+				for (int i = 0; i < size; i++) {
+					System.out.print(queue[(front + i) % 3] + " ");
+				}
+			   }
+			}
+
+		
+
+		public int getSize() {
+			return size;
+		}
+
+		public boolean isFull() {
+			return getSize() == 5;
+		}
+
+		public boolean isEmpty() {
+			return getSize() == 0;
+		}
+	}
+
+
---
+++ src/com/Queue/RunnerQueue.java
@@ -0,0 +1,14 @@
+package com.Queue;
+
+public class RunnerQueue {
+
+		public static void main(String[] args) {
+			QueueList q=new QueueList();
+			q.enQueue(56);
+			q.enQueue(30);
+			q.enQueue(70);
+			
+			q.show();
+		}
+
+}
